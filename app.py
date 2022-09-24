98f27ba0b055ee23a1f53fb43c325ef42e23aed6
SijoonKim1919 <87267309+SijoonKim1919@users.noreply.github.com>
2022-09-24 20:12:59 +0900

    Update app.py

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ def load_wordmodel(model):
     
     wordmodel=models.KeyedVectors.load_word2vec_format(model, binary=True)
     return wordmodel
-wordmodel=load_wordmodel('GoogleNews-vectors-negative300.bin.gz')
+wordmodel=load_wordmodel('https://s3.amazonaws.com/dl4j-distribution/GoogleNews-vectors-negative300.bin.gz')
 def word_to_num(w):
  
   p=word_tokenize(w)
